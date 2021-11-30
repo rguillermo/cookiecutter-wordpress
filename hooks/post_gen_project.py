0ce8e22cfc8e031bd5c9eb82d29dffda55003f28
rguillermo <jguillermo.dev@gmail.com>
2021-11-30 08:43:01 -0500

    fix: Post generation path

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,7 +1,7 @@
 import os
 
 wp_dir = 'wp_' + '{{cookiecutter.project_slug}}'
-root_dir = os.path.dirname(os.path.dirname(__file__))
+root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 package_dir = os.path.join(root_dir, wp_dir)
 package_path = os.path.join(package_dir, 'package.json')
 
