048c097268cc9aa4ffec8ecb1cf81189b55105e7
rguillermo <jguillermo.dev@gmail.com>
2021-11-30 08:35:47 -0500

    fix: Fix hook

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,8 @@
+import os
+
+wp_dir = 'wp_' + '{{cookiecutter.project_slug}}'
+root_dir = os.path.dirname(os.path.dirname(__file__))
+package_dir = os.path.join(root_dir, wp_dir)
+package_path = os.path.join(package_dir, 'package.json')
+
+os.rename(package_path, root_dir)
--- hooks/post_gen_project.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-PACKAGE_DIR="$(dirname -- "$SCRIPT_DIR")/{{ cookiecuter.project_slug }}"
-ROOT_DIR="$(dirname -- "$SCRIPT_DIR")"
-
-mv "$PACKAGE_DIR/package.json" "$ROOT_DIR"
